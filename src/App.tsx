e961f01f2f1f46487676fd819c8d0d10e7778a51
syun_m <sm.net_minecraft.hacker@outlook.jp>
2023-11-14 01:00:56 +0900

    名前の修正

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
       <div className="App">
           <img src="./images/logo.png" className="App-logo" alt="logo" width={240} height={240} />
       </div>
-      <h1>Batrachoxin</h1>
+      <h1>Batrachotoxin</h1>
       <div className="card">
         <p>
           Welcome to Batora's website!
