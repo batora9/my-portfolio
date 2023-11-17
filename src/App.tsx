db6eff1b72dc0faf54fc3536255a0bc1262a0828
syun_m <sm.net_minecraft.hacker@outlook.jp>
2023-11-17 17:23:27 +0900

    Xのロゴ表示と微修正

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,12 @@ function App() {
       <div className="App">
           <img src="./images/logo.png" className="App-logo" alt="logo" width={240} height={240} />
       </div>
-      <h1>Batrachotoxin</h1>
+      <h1>Batora</h1>
+      <a href="https://x.com/265" target="_blank"> 
+        <img src="./images/x_icon.svg" className="App-logo" alt="link" width={40} height={40} />
+      </a>
+      <h2>Software / Web Engineer</h2>
       <div className="card">
-        <p>
-          Welcome to Batora's website!
-        </p>
       </div>
     </>
   );
