492d9b2c12f0e9237fa3bb2e6ea6c607a203cffd
syun_m <sm.net_minecraft.hacker@outlook.jp>
2023-12-08 18:23:16 +0900

    aboutページ修正

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react"
 import { Link } from "react-router-dom"
+import { Container } from '@mui/material';
 
 export const About = () => {
     useEffect(() => {
@@ -7,10 +8,12 @@ export const About = () => {
       }, []);
 
     return (
-      <div>
-        <h3>About page is now under construction.</h3>
-        <Link to="/">Topに戻る</Link>
-      </div>
+      <Container maxWidth="md" sx={{ pt: 4, pb: 4 }}>
+        <div>
+          <h3>About page is now under construction.</h3>
+          <Link to="/">Topに戻る</Link>
+        </div>
+      </Container>
     )
   }
 
