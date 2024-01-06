d725a74c8755b023efe9728671650fa2eac8d89b
syun_m <sm.net_minecraft.hacker@outlook.jp>
2024-01-06 13:31:11 +0900

    ログアウト機能の実装

--- client/src/pages/admin-home.tsx
+++ client/src/pages/admin-home.tsx
@@ -1,15 +1,35 @@
-import { useEffect } from 'react';
-import { Container } from '@mui/material';
+import { useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthUserContext } from '../providers/user';
+import { Button, Container } from '@mui/material';
 
 export const Admin = () => {
     useEffect(() => {
         document.title = '管理者画面'; // タイトルを変更する
     }, []);
 
+    const { updateUser } = useContext(AuthUserContext);
+
+    const handleLogout = async () => {
+        // ログアウト処理
+        localStorage.removeItem('token');
+        // ユーザー情報を更新する
+        await updateUser();
+        // ログイン画面に遷移する
+        window.location.href = '/login';
+    }
+
     return(
         <div>
             <Container maxWidth="md" sx={{ pt: 4, pb: 4 }}>
             <h1>管理画面</h1>
+            <Button 
+            variant="contained"
+            size="medium"
+            onClick={handleLogout}
+          >
+              ログアウト
+          </Button>
             </Container>
         </div>
     )
