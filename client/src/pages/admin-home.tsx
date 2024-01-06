b18aa71abe76bbefb99b8a6e6477af1c653ed3bf
syun_m <sm.net_minecraft.hacker@outlook.jp>
2024-01-06 14:32:29 +0900

    管理画面のボタン作成

--- client/src/pages/admin-home.tsx
+++ client/src/pages/admin-home.tsx
@@ -1,37 +1,47 @@
-import { useContext, useEffect } from 'react';
-import { AuthUserContext } from '../providers/user';
-import { Button, Container } from '@mui/material';
+import { useContext, useEffect } from "react";
+import { AuthUserContext } from "../providers/user";
+import { Button, Container } from "@mui/material";
+import { Box } from "@mui/system";
 
 export const Admin = () => {
-    useEffect(() => {
-        document.title = '管理者画面'; // タイトルを変更する
-    }, []);
+  useEffect(() => {
+    document.title = "管理者画面"; // タイトルを変更する
+  }, []);
 
-    const { updateUser } = useContext(AuthUserContext);
+  const { updateUser } = useContext(AuthUserContext);
 
-    const handleLogout = async () => {
-        // ログアウト処理
-        localStorage.removeItem('token');
-        // ユーザー情報を更新する
-        await updateUser();
-        // ログイン画面に遷移する
-        window.location.href = '/login';
-    }
+  const handleLogout = async () => {
+    // ログアウト処理
+    localStorage.removeItem("token");
+    // ユーザー情報を更新する
+    await updateUser();
+    // ログイン画面に遷移する
+    window.location.href = "/login";
+  };
 
-    return(
-        <div>
-            <Container maxWidth="md" sx={{ pt: 4, pb: 4 }}>
-            <h1>管理画面</h1>
-            <Button 
-            variant="contained"
-            size="medium"
-            onClick={handleLogout}
-          >
-              ログアウト
+  return (
+    <div>
+      <Container maxWidth="md" sx={{ pt: 4, pb: 4 }}>
+        <h1>管理画面</h1>
+        <Box sx={{ "& > :not(style)": { m: 3 } }} component="form">
+          <Button variant="contained" size="large">
+            新規記事作成
           </Button>
-            </Container>
-        </div>
-    )
-}
+          <Button variant="contained" size="large">
+            記事管理
+          </Button>
+          <Button variant="contained" size="large">
+            ユーザー管理
+          </Button>
+        </Box>
+        <Box sx={{ "& > :not(style)": { m: 3 } }} component="form">
+          <Button variant="contained" size="medium" onClick={handleLogout}>
+            ログアウト
+          </Button>
+        </Box>
+      </Container>
+    </div>
+  );
+};
 
 export default Admin;
